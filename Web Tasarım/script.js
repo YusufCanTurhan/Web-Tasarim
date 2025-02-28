0d5606b9b7b171706b1bdd16c81c937eb414d1f7
Rookie78 <78yusufcan78@gmail.com>
2025-02-28 23:27:19 +0300

    Add files via upload

---
+++ Web Tasarım/script.js
@@ -0,0 +1,7 @@
+
+function formGoster() {
+            document.getElementById("iletisimFormu").style.display = "block";
+        }
+        function formKapat() {
+            document.getElementById("iletisimFormu").style.display = "none";
+        }
